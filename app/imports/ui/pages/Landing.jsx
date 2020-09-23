ef5f0fa37ff9218e885a47db09d7c4908429d161
ryan_ell <ryan.ell42@gmail.com>
2020-09-22 17:50:10 -0700

    added to the landing page
    some ideas to be implemented

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Button, Container, Grid, Image } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -8,14 +9,35 @@ class Landing extends React.Component {
         <Grid verticalAlign='middle' textAlign='center' container>
 
           <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
+            <Image size='large' circular src="/images/dpr-logo.png"/>
           </Grid.Column>
-
           <Grid.Column width={8}>
-            <h1>Aloha</h1>
-            <p>Due to COVID-19, the Kaneohe District Park Pool is only open for lap swimming via reservations at this
-              time. This is a mock up of a website that could be used to check the available swim slots as well as make
-              reservations to swim.</p>
+            <div>
+              <h1>Aloha</h1>
+              <p>Due to COVID-19, the Kaneohe District Park Pool is only open for lap swimming via reservations at this
+                time. This is a mock up of a website that could be used to check the available swim slots as well as
+                make
+                reservations to swim.</p>
+            </div>
+            <Container>
+                <p>In order to make a reservation please make an account or sign in</p>
+              <div>
+                <Button as={NavLink} activeClassName="" exact to="/signin"
+                        size='medium' color='black'
+                        className='ui button' floated='right'>Sign in</Button>
+                <Button as={NavLink} activeClassName="" exact to="/signup"
+                        size='medium' color='black'
+                        className='ui button' floated='right'>Create Account</Button>
+              </div>
+            </Container>
+            <Container>
+              <div>
+                <p>
+                  Hours of operation
+                </p>
+                <Image size='medium' circular src="/images/pool-hours.png"/>
+              </div>
+            </Container>
           </Grid.Column>
 
         </Grid>
